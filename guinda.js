0ae2e1b0763440cee239abf69f145913b4c28222
Luciano Iam <oss@lucianoiam.com>
2021-07-09 13:13:24 +0200

    Bugfix: cursor on resize handle release

--- guinda.js
+++ guinda.js
@@ -387,6 +387,8 @@ function ControlTrait() {
         ev.originalEvent = originalEvent;
 
         // Copy some standard properties
+        ev.clientX = originalEvent.clientY;
+        ev.clientY = originalEvent.clientY;
         ev.shiftKey = originalEvent.shiftKey;
         ev.ctrlKey = originalEvent.ctrlKey;
 
@@ -506,17 +508,22 @@ class ResizeHandle extends InputWidget {
         this.addEventListener('controlstart', this._onGrab);
         this.addEventListener('controlcontinue', this._onDrag);
 
-        const unsetCursor = () => {
-            if (!this.isControlStarted) {
+        const unserCursorIfNeeded = (ev) => {
+            if (!this.isControlStarted && !this._isMouseIn) {
                 document.body.style.cursor = null;
             }
         };
 
-        this.addEventListener('controlend', unsetCursor);
-        this.addEventListener('mouseleave', unsetCursor);
+        this.addEventListener('controlend', unserCursorIfNeeded);
 
         this.addEventListener('mouseenter', (ev) => {
+            this._isMouseIn = true;
             document.body.style.cursor = 'nwse-resize';
+        });
+
+        this.addEventListener('mouseleave', () => {
+            this._isMouseIn = false;
+            unserCursorIfNeeded();
         });
     }
 
